4e3beebd96783d8c314b282f7197f0f48fa49e09
Caleb Hoff <crunchycat1@gmail.com>
2024-12-13 15:06:49 -0600

    fix privacy policy address color, add labeledbanner blur, fix 1 blur in <saf18, optimize SVGs, update deps

--- src/components/LabeledBanner/index.tsx
+++ src/components/LabeledBanner/index.tsx
@@ -3,7 +3,7 @@ import styles from '@/components/LabeledBanner/LabeledBanner.module.scss';
 
 export const LabeledBanner = ([ImgUrl, label, aspectRatio]: [string, string, string]) => (
   <div class={styles.LabeledBanner}>
-    <span>{label}</span>
+    <div>{label}</div>
     {ImageSample(ImgUrl, label, aspectRatio)}
   </div>
 );
